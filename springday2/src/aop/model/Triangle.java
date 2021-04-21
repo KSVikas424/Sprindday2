d46beb32270ef539335c4e7374af1dc0dd968306
ksvikas424 <srinivasvikask@gmail.com>
2021-04-21 10:13:52 +0530

    @Before

---
+++ springday2/src/aop/model/Triangle.java
@@ -0,0 +1,17 @@
+package aop.model;
+
+public class Triangle {
+	String name;
+	
+	public Triangle() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
